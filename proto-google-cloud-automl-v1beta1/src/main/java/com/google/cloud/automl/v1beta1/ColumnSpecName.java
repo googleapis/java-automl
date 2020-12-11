8e80402c93f45dd898c5ea60a8a8b29c8f117575
yoshi-automation <yoshi-automation@google.com>
2020-12-10 16:04:47 -0800

    fix: Update gapic-generator-java to 0.0.7
    Committer: @miraleung
    PiperOrigin-RevId: 345476969
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Dec 3 10:07:32 2020 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 7be2c821dd88109038c55c89f7dd48f092eeab9d
    Source-Link: https://github.com/googleapis/googleapis/commit/7be2c821dd88109038c55c89f7dd48f092eeab9d

--- proto-google-cloud-automl-v1beta1/src/main/java/com/google/cloud/automl/v1beta1/ColumnSpecName.java
+++ proto-google-cloud-automl-v1beta1/src/main/java/com/google/cloud/automl/v1beta1/ColumnSpecName.java
@@ -133,7 +133,7 @@ public class ColumnSpecName implements ResourceName {
   public static List<String> toStringList(List<ColumnSpecName> values) {
     List<String> list = new ArrayList<>(values.size());
     for (ColumnSpecName value : values) {
-      if (Objects.isNull(value)) {
+      if (value == null) {
         list.add("");
       } else {
         list.add(value.toString());
@@ -148,23 +148,23 @@ public class ColumnSpecName implements ResourceName {
 
   @Override
   public Map<String, String> getFieldValuesMap() {
-    if (Objects.isNull(fieldValuesMap)) {
+    if (fieldValuesMap == null) {
       synchronized (this) {
-        if (Objects.isNull(fieldValuesMap)) {
+        if (fieldValuesMap == null) {
           ImmutableMap.Builder<String, String> fieldMapBuilder = ImmutableMap.builder();
-          if (!Objects.isNull(project)) {
+          if (project != null) {
             fieldMapBuilder.put("project", project);
           }
-          if (!Objects.isNull(location)) {
+          if (location != null) {
             fieldMapBuilder.put("location", location);
           }
-          if (!Objects.isNull(dataset)) {
+          if (dataset != null) {
             fieldMapBuilder.put("dataset", dataset);
           }
-          if (!Objects.isNull(tableSpec)) {
+          if (tableSpec != null) {
             fieldMapBuilder.put("table_spec", tableSpec);
           }
-          if (!Objects.isNull(columnSpec)) {
+          if (columnSpec != null) {
             fieldMapBuilder.put("column_spec", columnSpec);
           }
           fieldValuesMap = fieldMapBuilder.build();
