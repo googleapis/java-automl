2439919d6824b9bdc100b7c96b8d7bef0fddae4c
Mike Ganbold <munkhbayarg@google.com>
2021-04-12 16:10:16 -0700

    chore: added cleanup option in the setup ListOperationIT

--- samples/snippets/src/test/java/com/example/automl/ListOperationStatusTest.java
+++ samples/snippets/src/test/java/com/example/automl/ListOperationStatusTest.java
@@ -19,9 +19,16 @@ package com.example.automl;
 import static com.google.common.truth.Truth.assertThat;
 import static junit.framework.TestCase.assertNotNull;
 
+import com.google.cloud.automl.v1.AutoMlClient;
+import com.google.cloud.automl.v1.LocationName;
+import com.google.longrunning.ListOperationsRequest;
+import com.google.longrunning.Operation;
+import com.google.longrunning.OperationsClient;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -49,11 +56,35 @@ public class ListOperationStatusTest {
   }
 
   @Before
-  public void setUp() {
+  public void setUp() throws IOException {
     bout = new ByteArrayOutputStream();
     out = new PrintStream(bout);
     originalPrintStream = System.out;
     System.setOut(out);
+
+    // if the LRO status count more than 300, delete all operations.
+    try (AutoMlClient client = AutoMlClient.create()) {
+      OperationsClient operationsClient = client.getOperationsClient();
+      LocationName projectLocation = LocationName.of(PROJECT_ID, "us-central1");
+      ListOperationsRequest listRequest =
+          ListOperationsRequest.newBuilder().setName(projectLocation.toString()).build();
+      List<String> operationFullPathsToBeDeleted = new ArrayList<>();
+      for (Operation operation :
+          operationsClient.listOperations(listRequest).iterateAll()) {
+          // collect unused operation into the list.
+          operationFullPathsToBeDeleted.add(operation.getName());
+      }
+
+      if(operationFullPathsToBeDeleted.size() > 300) {
+        for (String operationFullPath : operationFullPathsToBeDeleted) {
+          // delete unused operations.
+          operationsClient.deleteOperation(operationFullPath);
+        }
+      }else{
+        // Clear the list since we wont anything with the list.
+        operationFullPathsToBeDeleted.clear();
+      }
+    }
   }
 
   @After
